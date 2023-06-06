7fba8bfd11bf658c15badf79d2fb663f2d3416bb
renarasecchim <renarasecchim@gmail.com>
2023-06-06 12:40:19 -0300

    chore: init course java

---
+++ main.java
@@ -0,0 +1,17 @@
+public class main {
+
+  public static void main(String[] args){
+    String name = "Renara";
+    // number decimal
+    double doublNumber = (double) 8; 
+    //interval usually
+    double intNumber = (double) 9;
+
+    double division = intNumber / doublNumber;
+
+    System.out.println("Olá " + name);
+    System.out.println("Number decimal " + doublNumber);
+    System.out.println("Number " + intNumber);
+    System.out.println(division);
+  }
+}
