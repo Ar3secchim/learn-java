2b20d1c5fd43c1d7f48b77798b44bfc603a4b1bb
renarasecchim <renarasecchim@gmail.com>
2023-06-19 10:27:25 -0300

    update: classes curse

---
+++ conditional.java
@@ -0,0 +1,11 @@
+public class conditional {
+  public static void main(String[] args){
+    int nota = 90; 
+
+    if(nota >= 70){
+      System.out.println("Aluno aprovado");
+    }else{
+      System.out.println("Aluno reprovado");
+    }
+  }
+}
---
+++ logicos.java
@@ -0,0 +1,10 @@
+public class logicos {
+   public static void main(boolean[] args) {
+    //boolean operadores logicos
+    boolean weekEnd = true;
+    boolean makeSun = true;
+
+    System.out.println(weekEnd == makeSun);
+  }
+
+}
